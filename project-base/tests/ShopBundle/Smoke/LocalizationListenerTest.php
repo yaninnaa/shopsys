fa276af1d0aeb9c7b2488f60d592abbe559cee18
Miroslav Sustek <miroslav.sustek@shopsys.com>
2017-03-02 12:04:15 +0100

    switch the 1st front-end domain to Czech localization and the 2nd front-end domain to English

--- project-base/tests/ShopBundle/Smoke/LocalizationListenerTest.php
+++ project-base/tests/ShopBundle/Smoke/LocalizationListenerTest.php
@@ -10,7 +10,7 @@ use Tests\ShopBundle\Test\DatabaseTestCase;
 
 class LocalizationListenerTest extends DatabaseTestCase
 {
-    public function testProductDetailOnFirstDomainHasEnglishLocale()
+    public function testProductDetailOnFirstDomainHasCzechLocale()
     {
         $router = $this->getServiceByType(CurrentDomainRouter::class);
         /* @var $router \Shopsys\ShopBundle\Component\Router\CurrentDomainRouter */
@@ -21,14 +21,14 @@ class LocalizationListenerTest extends DatabaseTestCase
         $this->assertSame(200, $this->getClient()->getResponse()->getStatusCode());
         $this->assertGreaterThan(
             0,
-            $crawler->filter('html:contains("Catalogue number")')->count()
+            $crawler->filter('html:contains("Katalogové číslo")')->count()
         );
     }
 
     /**
      * @group multidomain
      */
-    public function testProductDetailOnSecondDomainHasCzechLocale()
+    public function testProductDetailOnSecondDomainHasEnglishLocale()
     {
         $domain = $this->getServiceByType(Domain::class);
         /* @var $domain \Shopsys\ShopBundle\Component\Domain\Domain */
@@ -43,7 +43,7 @@ class LocalizationListenerTest extends DatabaseTestCase
         $this->assertSame(200, $this->getClient()->getResponse()->getStatusCode());
         $this->assertGreaterThan(
             0,
-            $crawler->filter('html:contains("Katalogové číslo")')->count()
+            $crawler->filter('html:contains("Catalogue number")')->count()
         );
     }
 }
